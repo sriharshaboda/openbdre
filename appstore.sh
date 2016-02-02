58322fb18fbb2bf2957770a6375fd0018a179430
Sri Harsha Boda <bodasriharsha@gmail.com>
2016-02-02 17:12:04 +0530

    adding appstore page, and renaming repo and app directory in the script

--- appstore.sh
+++ appstore.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-BDRE_APPSTORE_REPO=~/bdreappstore
-BDRE_APPSTORE_HOME=~/appstore-apps
+BDRE_APPSTORE_REPO=~/bdreappstore-repo
+BDRE_APPSTORE_APPS=~/bdreappstore-apps
 
-rm -f -r $BDRE_APPSTORE_HOME
-mkdir -p $BDRE_APPSTORE_HOME
+rm -f -r $BDRE_APPSTORE_APPS
+mkdir -p $BDRE_APPSTORE_APPS
 
 #Pull latest apps from the repo
 if [ -d "$BDRE_APPSTORE_REPO" ]; then
@@ -17,7 +17,7 @@ fi
 else
 echo "cloning repo for first time"
 cd ~
-git clone https://github.com/sriharshaboda/bdreappstore.git
+git clone https://github.com/sriharshaboda/bdreappstore-repo.git
 if [ $? -ne 0 ]
 then exit 1
 fi
@@ -36,8 +36,8 @@ for d in */ ; do
         fi
         echo "archiving done"
         done
-    mkdir -p $BDRE_APPSTORE_HOME/"$d"
-    rm -f $BDRE_APPSTORE_HOME/"$d"/*.zip
-    mv ./*.zip $BDRE_APPSTORE_HOME/"$d"
+    mkdir -p $BDRE_APPSTORE_APPS/"$d"
+    rm -f $BDRE_APPSTORE_APPS/"$d"/*.zip
+    mv ./*.zip $BDRE_APPSTORE_APPS/"$d"
         cd ../
 done
